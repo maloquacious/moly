1b0fbc20a601438c764e3c5d538f1eca8853ffbd
Michael Henderson <mdhender@mdhender.com>
2022-12-15 14:30:58 -0700

    added main and engine

---
+++ engine/loop.go
@@ -0,0 +1,40 @@
+/*
+ * moly - a game engine inspired by better games
+ * Copyright (C) 2022 Michael D Henderson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package engine
+
+import (
+	"fmt"
+	"log"
+)
+
+// Loop is the game loop.
+func (e *Engine) Loop() error {
+	for tick := 1; tick <= e.ticksPerTurn; tick++ {
+		log.Printf("loop: processing tick %d\n", tick)
+		if err := e.tick(tick); err != nil {
+			return fmt.Errorf("loop: %w", err)
+		}
+	}
+	return nil
+}
+
+// tick is one iteration of the game loop
+func (e *Engine) tick(tick int) error {
+	return	nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/maloquacious/moly
 
-go 1.17
+go 1.19
+
+require github.com/joho/godotenv v1.4.0
